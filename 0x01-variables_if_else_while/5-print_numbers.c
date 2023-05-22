fa6ef656f8b37f261c2a99380f2767617c36306e
PHILLIP <kantundaphillip@gmail.com>
2023-05-22 16:31:32 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d \n", a);
+	}
+		return (0);
+}
+
