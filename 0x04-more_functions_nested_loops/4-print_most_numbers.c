19cf91b06c9d1bbd2fdd89d82db2ef605389a809
PHILLIP <kantundaphillip@gmail.com>
2023-05-25 16:11:28 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_most_numbers - print number 0-9
+ *  Return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; 0 <= 9; c++)
+	{
+		if (!(c == 2 || c == 4))
+		_putchar(0);
+	}
+	_putchar('\n');
+}
+
