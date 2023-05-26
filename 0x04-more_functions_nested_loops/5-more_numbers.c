5bd9d26acb404b1abe6102c004a6b7ed8ede98a5
PHILLIP <kantundaphillip@gmail.com>
2023-05-25 17:03:14 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times the numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char c, f;
+
+	for (c = '0'; '0' <= '10'; c++)
+	{
+		for (f = '0'; '0' <= '14'; f++)
+		{
+			_putchar(f);
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
